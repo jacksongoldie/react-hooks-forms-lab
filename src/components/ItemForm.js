c308908dcb15af2279e59b19388271ca7dcea412
Goldie <goldiejackson25@yahoo.com>
2021-10-10 16:20:02 -0500

    commit tree 96cb62ae20944358ada65cabf6a115f4672938a5

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,39 @@
-import React from "react";
+import { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+
+ const [formData, setFormData] = useState({
+   id: uuid(),
+   name: '',
+   category: 'Produce'});
+
+  function handleChange(e){
+    let keyToChange = e.target.name;
+    let newThing = e.target.value;
+
+    //use name key to figure out what input is firing ---> e.target.name
+    setFormData({...formData, [keyToChange]: newThing})
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    //add data to state that adds shopping list
+    onItemFormSubmit(formData)
+    debugger;
+  }
+
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={formData.name} onChange={handleChange}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select value={formData.category} name="category" onChange={handleChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
